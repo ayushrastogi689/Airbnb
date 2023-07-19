25fdac4bd664ee0bc44642f5edb879c3c16175c5
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-19 20:07:51 +0530

    Created a Navbar and a container component

---
+++ airbnb-clone/src/app/components/Container.tsx
@@ -0,0 +1,13 @@
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({children}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Container
