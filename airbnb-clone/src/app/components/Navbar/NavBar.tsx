db97f65409f77455ffc34a362edf007368264c27
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-20 15:57:14 +0530

    Created a new NavBar component

---
+++ airbnb-clone/src/app/components/Navbar/NavBar.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Container from "../Container"
+import Logo from "./Logo/Logo"
+import Search from "./Search/Search"
+import UserMenu from "./UserMenu/UserMenu"
+import Categories from "./Categories/Categories"
+
+const NavBar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+          <div className="py-4 border-b[1px]">
+        <Container >
+          <div className=" flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+         </div>
+        </Container>              
+      </div>
+      <Categories />
+    </div>
+  )
+}
+
+export default NavBar
--- airbnb-clone/src/app/page.tsx
+++ airbnb-clone/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-2xl">
-    Hello Airbnb  
+     Airbnb  
     </div>
   )
 }
