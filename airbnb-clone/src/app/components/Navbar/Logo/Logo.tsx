fb5ccb3dd76b30e39c560e9241f08b17438d78fa
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-20 22:15:21 +0530

    Added home link to airbnb logo

--- airbnb-clone/src/app/components/Navbar/Logo/Logo.tsx
+++ airbnb-clone/src/app/components/Navbar/Logo/Logo.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation"
 const Logo = () => {
     const router = useRouter();
     return (
-    <Image className="hidden md:block cursor-pointer" alt="logo-image" height="100" width = "100" src="/images/logo.png"/>
+    <Image onClick={() => router.push("/")} className="hidden md:block cursor-pointer" alt="logo-image" height="100" width = "100" src="/images/logo.png"/>
   )
 }
 
