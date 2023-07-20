1fb28b9b4b72e068b354608e95e9b2c866d5c858
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-20 11:12:33 +0530

    Added UserAvatar & HamburgerMenu Icon in UserMenu

--- airbnb-clone/src/app/components/Navbar/UserMenu/UserMenu.tsx
+++ airbnb-clone/src/app/components/Navbar/UserMenu/UserMenu.tsx
@@ -1,4 +1,6 @@
 'use client'
+import {AiOutlineMenu} from "react-icons/ai"
+import UserAvatar from "../../UserAvatar/UserAvatar"
 
 const UserMenu = () => {
   return (
@@ -10,7 +12,10 @@ const UserMenu = () => {
                 Airbnb your home                  
               </div>
               <div className="p-4 md:px-2 md:py-1 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition " >
-                  
+                  <AiOutlineMenu />
+                  <div className="hidden md:block">
+                      <UserAvatar />
+                  </div>
               </div>
           </div>
       
