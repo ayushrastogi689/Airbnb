58aa44fa34a59284dfb374aa98aa84d0e3e58901
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-20 16:02:15 +0530

    Added NavBar component to layout

--- airbnb-clone/src/app/layout.tsx
+++ airbnb-clone/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
-
+import NavBar from './components/Navbar/NavBar'
 const font = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <NavBar />
         {children}
       </body>
     </html>
