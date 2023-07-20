0003219fd17e3b9a9b10ae0ea3d7a4835cde8ecf
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-20 16:30:44 +0530

    Added multiple categories of destinations

--- airbnb-clone/src/app/components/Navbar/Categories/Categories.tsx
+++ airbnb-clone/src/app/components/Navbar/Categories/Categories.tsx
@@ -3,9 +3,11 @@
 import Container from "../../Container"
 import CategoriesBox from "../../CategoriesBox/CategoriesBox"
 
-import { TbBeach } from "react-icons/tb"
-import { GiWindmill } from "react-icons/gi"
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb"
+import { GiWindmill, GiIsland, GiBoatFishing, GiCastle, GiForestCamp } from "react-icons/gi"
 import {MdOutlineVilla} from "react-icons/md"
+import {FaSkiing} from "react-icons/fa"
+
 import { usePathname, useSearchParams } from "next/navigation"
 export const categories = [
     {
@@ -16,12 +18,47 @@ export const categories = [
     {
         icon: GiWindmill,
         label: "Windmills",
-        description: "This property is close to beach"
+        description: "This property is close to Windmill"
     },
     {
         icon: MdOutlineVilla,
         label: "Modern",
         description: "This property is a Modern house"
+    },
+    {
+        icon: TbMountain,
+        label: "Countryside",
+        description: "This property is in the countryside"
+    },
+    {
+        icon: TbPool,
+        label: "Pools",
+        description: "This property has a pool inside"
+    },
+    {
+        icon: GiIsland,
+        label: "Islands",
+        description: "This property is on a Island"
+    },
+    {
+        icon: GiBoatFishing,
+        label: "Lake",
+        description: "This property is close to lake"
+    },
+    {
+        icon: FaSkiing,
+        label: "Skiing",
+        description: "This property has skiing activities"
+    },
+    {
+        icon: GiCastle,
+        label: "Castle",
+        description: "This property is in a castle "
+    },
+    {
+        icon: GiForestCamp,
+        label: "Camp",
+        description: "This property has camping activities"
     },
 ]
 
