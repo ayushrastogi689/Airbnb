93e05925c9e53cca1ed5365ba10059db97ac5ff4
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-20 15:35:38 +0530

    Dependency - Installed query string package

--- airbnb-clone/src/app/components/Navbar/UserMenu/UserMenu.tsx
+++ airbnb-clone/src/app/components/Navbar/UserMenu/UserMenu.tsx
@@ -1,6 +1,6 @@
 'use client'
 import {AiOutlineMenu} from "react-icons/ai"
-import UserAvatar from "../../UserAvatar/UserAvatar"
+import UserAvatar from "../UserAvatar/UserAvatar"
 import { useCallback, useState } from "react"
 import MenuItem from "../MenuItem/MenuItem"
 
@@ -31,9 +31,9 @@ const UserMenu = () => {
           {isOpen && (<div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
               <div className="flex flex-col cursor-pointer ">
                   <>
-                      <MenuItem onClick={}
+                      <MenuItem 
                       label = "Sign Up" />
-                      <MenuItem onClick={}
+                      <MenuItem 
                       label = "Login" />
                   </>
               </div>
